58f3529811766bc6ac0da676e6313c2a4da8cab9
Nyhfox <87608126+Nyhfox@users.noreply.github.com>
2023-03-17 20:03:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ noBtn.addEventListener('mouseover',() =>{
    noBtn.style.top = j + 'px';
 
 });
+
+noBtn.addEventListener('click',() =>{
+   const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+   const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+
+   noBtn.style.left = i + 'px';
+   noBtn.style.top = j + 'px';
+
+});
